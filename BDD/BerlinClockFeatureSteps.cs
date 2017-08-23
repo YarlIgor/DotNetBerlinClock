58a650a62d506315a61c70dd2298c38a07df8cf1
Igor Kovtun <igor.kovtun@gmail.com>
2017-08-23 11:50:06 +0300

    Add gitignore, Add logic to handle incorrect input time, Use Assert.That instead of Assert.Equals

--- BDD/BerlinClockFeatureSteps.cs
+++ BDD/BerlinClockFeatureSteps.cs
@@ -1,28 +1,34 @@
 ﻿using System;
 using TechTalk.SpecFlow;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
+using NUnit.Framework;
 
 namespace BerlinClock
 {
     [Binding]
     public class TheBerlinClockSteps
     {
-        private ITimeConverter berlinClock = new TimeConverter();
-        private String theTime;
+        private ITimeConverter _berlinClock = new TimeConverter();
+        private String _inputTime;
 
-        
+
         [When(@"the time is ""(.*)""")]
         public void WhenTheTimeIs(string time)
         {
-            theTime = time;
+            _inputTime = time;
         }
-        
+
         [Then(@"the clock should look like")]
         public void ThenTheClockShouldLookLike(string theExpectedBerlinClockOutput)
         {
-            Assert.AreEqual(berlinClock.convertTime(theTime), theExpectedBerlinClockOutput);
+            Assert.That(_berlinClock.convertTime(_inputTime), Is.EqualTo(theExpectedBerlinClockOutput));
         }
 
+        [Then(@"the user should see an error message ""(.*)""")]
+        public void ThenTheUserShouldSeeAnError(string errorMessage)
+        {
+            var exception = Assert.Throws<BerlinClockInputTimeFormatException>(() => _berlinClock.convertTime(_inputTime));
+            Assert.That(exception.Message, Is.EqualTo(errorMessage));
+        }
     }
 }
